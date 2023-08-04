d17b6ed1d259f90baa78f6232b27567a9981593a
mmdzov <mzov939@gmail.com>
2023-08-05 03:12:16 +0430

    Update

--- auto.sh
+++ auto.sh
@@ -61,8 +61,9 @@ wget -O "$assets/iran.dat" https://github.com/bootmortis/iran-hosted-domains/rel
 
 xray_config="/var/lib/marzban/xray_config.json"
 
-routing_file="https://github.com/mmdzov/auto-conf-marzban/raw/main/routing.json"
-outbounds_file="https://github.com/mmdzov/auto-conf-marzban/raw/main/routing.json"
+
+routing_file=$(curl -s "https://github.com/mmdzov/auto-conf-marzban/raw/main/routing.json" | jq -c '.')
+outbounds_file=$(curl -s "https://github.com/mmdzov/auto-conf-marzban/raw/main/outbounds.json" | jq -c '.')
 
 jq --argfile routing "$routing_file" '.routing = $routing' "$xray_config" > tmp.json && mv tmp.json "$xray_config"
 jq --argfile outbounds "$outbounds_file" '.outbounds = $outbounds' "$xray_config" > tmp.json && mv tmp.json "$xray_config"
