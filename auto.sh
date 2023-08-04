2996a5101a4950c29d8eced30e6657172171926e
mmdzov <mzov939@gmail.com>
2023-08-04 20:13:25 +0430

    Update

--- auto.sh
+++ auto.sh
@@ -151,11 +151,11 @@ read -p "Please enter your telegram api token: " telegram_api_token
 read -p "Please enter your telegram user id: " telegram_user_id
 
 if [[ -n $telegram_api_token ]]; then
-    sed -i "s/# TELEGRAM_API_TOKEN = .*/TELEGRAM_API_TOKEN = \"$telegram_api_token\"/" $env
+    update_env_variable "$env_file" "TELEGRAM_API_TOKEN" "$telegram_api_token"
 fi
 
 if [[ -n $telegram_user_id ]]; then
-    sed -i "s/# TELEGRAM_ADMIN_ID = .*/TELEGRAM_ADMIN_ID = \"$telegram_user_id\"/" $env
+    update_env_variable "$env_file" "TELEGRAM_ADMIN_ID" "$telegram_user_id"
 fi
 
 
